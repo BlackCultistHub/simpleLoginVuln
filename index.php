891a7ce10e4c27456bf3f97dd850d57b6b2dc54f
BlackCultist <tema-vil@bk.ru>
2021-06-07 20:08:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<h3>Choose Path</h3>
+  <form role="form" action="bruteforce.php" method="get">
+	<button type="submit" class="btn btn-success rounded-0">Bruteforce</button>
+  </form>
+  <form role="form" action="sql.php" method="get">
+	<button type="submit" class="btn btn-success rounded-0">Sql injections</button>
+  </form>
+  </body>
+</html>
